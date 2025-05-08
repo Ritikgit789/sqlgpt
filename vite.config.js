a5613aaed9a40516b1d536adb94bee7b4bbcc01c
Ritikgit789 <ritikmaity4@gmail.com>
2025-05-08 21:36:16 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // build: {
+//   //   rollupOptions: {
+//   //     external: []  // Externalize react-markdown
+//   //   }
+//   // }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     external: []  // Externalize react-markdown
-  //   }
-  // }
+  base: './',
 })
+
